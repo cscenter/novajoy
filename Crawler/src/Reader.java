f7d169a400c70145d89e48640f86e874a213cb43
Bassin <01kz01@gmail.com>
2013-03-23 23:24:24 +0400

    single thread crawler
    Crawler reads all rssfeeds in database, crawls them, then writes it's
    items in base and updates pubdates of rssfeed

--- Crawler/src/Reader.java
+++ Crawler/src/Reader.java
@@ -18,97 +18,98 @@ import com.sun.syndication.io.XmlReader;
 
 public class Reader {
 
-    public static void main(String[] args) throws Exception {
-        process_reqsts("novajoy.cosqmvrs3gb3.us-east-1.rds.amazonaws.com", "novajoydb", "novauser", "ru4Afoh5");
-    }
+	public static void main(String[] args) throws Exception {
+		process_reqsts("novajoy.cosqmvrs3gb3.us-east-1.rds.amazonaws.com",
+				"novajoydb", "novauser", "ru4Afoh5");
+	}
 
+	private static void process_reqsts(String srvrName, String dbName,
+			String usrName, String pass) {
+		Connection connection;
+		try {
+			connection = establish_jdbc_connection(srvrName, dbName, usrName,
+					pass);
 
-    private static void process_reqsts(String srvrName, String dbName, String usrName, String pass) {
-        Connection connection;
-        try {
-            connection = establish_jdbc_connection(srvrName, dbName, usrName, pass);
+			String query = "Select * FROM Server_rssfeed";
+			String get_query1 = "UPDATE Server_rssfeed  SET pubDate='";
+			String get_query2 = "' WHERE id=";
 
-            String query = "Select * FROM Server_rssfeed";
-            String get_query1 = "UPDATE Server_rssfeed  SET content='";
-            String get_query2 = "' WHERE id=";
+			Statement stmt = connection.createStatement();
+			PreparedStatement ps = connection.prepareStatement("");
+			ResultSet rs = stmt.executeQuery(query);
+			String addr;
+			while (rs.next()) {
+				addr = rs.getString(2);
+				System.out.println(addr);
+				SyndFeed feed = readfeed(addr);
+				for (Iterator i = feed.getEntries().iterator(); i.hasNext();) {
+					insert_item((SyndEntry) i.next(), connection,
+							rs.getString(1));
+				}
+				java.sql.Timestamp time = new java.sql.Timestamp(feed
+						.getPublishedDate().getTime());
+				String req = get_query1 + time + get_query2 + rs.getString(1)
+						+ ";";
+				ps.executeUpdate(req);
+				System.out.println(req);
+			}
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			// Could not find the database driver
+		} catch (Exception e) {
+			e.printStackTrace();
+			// Could not connect to the database
+		}
 
-            Statement stmt = connection.createStatement();
-            PreparedStatement ps = connection.prepareStatement("");
-            ResultSet rs = stmt.executeQuery(query);
-            String addr;
+	}
 
-            while (rs.next()) {
-                addr = rs.getString(2);
-                System.out.println(addr);
-                String req = get_query1 + readfeed(addr) + get_query2 + rs.getString(1) + ";";
-                ps.executeUpdate(req);
-                System.out.println(req);
-            } // end while
-            connection.close();
-        } // end try
-        catch (SQLException e) {
-            e.printStackTrace();
-            // Could not find the database driver
-        } catch (Exception e) {
-            e.printStackTrace();
-            // Could not connect to the database
-        }
+	private static Connection establish_jdbc_connection(String srvrName,
+			String dbName, String usrName, String pass)
+			throws ClassNotFoundException, SQLException {
+		String driverName = "com.mysql.jdbc.Driver";
 
-    }
+		Class.forName(driverName);
 
-    private static Connection establish_jdbc_connection(String srvrName, String dbName, String usrName, String pass) throws ClassNotFoundException, SQLException {
-        String driverName = "com.mysql.jdbc.Driver";
+		// Create a connection to the database
+		String url = "jdbc:mysql://" + srvrName + "/" + dbName;
 
-        Class.forName(driverName);
+		Connection connection = DriverManager.getConnection(url, usrName, pass);
 
-        // Create a connection to the database
-        String url = "jdbc:mysql://" + srvrName + "/" + dbName;
+		System.out.println("Connected to DB" + connection);
+		return connection;
+	}
 
-        Connection connection = DriverManager.getConnection(url, usrName, pass);
+	private static SyndFeed readfeed(String addr) throws Exception {
+		XmlReader reader = null;
+		SyndFeed feed = null;
+		try {
+			reader = new XmlReader(new URL(addr));
+			feed = new SyndFeedInput().build(reader);
+		} finally {
+			if (reader != null)
+				reader.close();
+		}
+		return feed;
+	}
 
-        System.out.println("Connected to DB" + connection);
-        return connection;
-    }
-
-
-    private static String readfeed(String addr) throws Exception {
-        URL url = new URL(addr);
-        XmlReader reader = null;
-        return (URLConnectionReader.getText(addr));
-        /*try {
-
-            reader = new XmlReader(url);
-            SyndFeed feed = new SyndFeedInput().build(reader);
-            System.out.println("Feed Title: " + feed.getAuthor());
-
-            for (Iterator i = feed.getEntries().iterator(); i.hasNext(); ) {
-                SyndEntry entry = (SyndEntry) i.next();
-                //System.out.println(entry.getTitle());
-            }
-
-        } finally {
-            if (reader != null)
-                reader.close();
-        }*/
-    }
-}
-
-class URLConnectionReader {
-    public static String getText(String url) throws Exception {
-        URL website = new URL(url);
-        URLConnection connection = website.openConnection();
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(
-                        connection.getInputStream()));
-
-        StringBuilder response = new StringBuilder();
-        String inputLine;
-
-        while ((inputLine = in.readLine()) != null)
-            response.append(inputLine);
-
-        in.close();
-
-        return response.toString();
-    }
+	private static void insert_item(SyndEntry entry, Connection connection,
+			String feedid) throws SQLException {
+		Statement stmt = connection.createStatement();
+		String query = "INSERT INTO Server_rssitem (rssfeed_id, title, description, link, author, pubDate) VALUES("
+				+ feedid
+				+ ",\""
+				+ entry.getTitle()
+				+ "\",\""
+				+ entry.getDescription().getValue().replace("\"", "\\\"")
+				+ "\",\""
+				+ entry.getLink()
+				+ "\",\""
+				+ entry.getAuthor()
+				+ "\",\""
+				+ (new java.sql.Timestamp(entry.getPublishedDate().getTime()))
+				+ "\");";
+		// System.out.println(query);
+		stmt.executeUpdate(query);
+	}
 }
