afa981778b51202fb54139b071ae58c3700024a8
Bassin <01kz01@gmail.com>
2013-03-27 19:56:26 +0400

    DBmanager class

---
+++ src/crawler/src/JdbcManager.java
@@ -0,0 +1,37 @@
+import java.sql.*;
+
+public class JdbcManager {
+	private Connection connection;
+	private String baseAddr, baseName, baseUser, basePass;
+
+	public JdbcManager(String str, String baseAddr, String baseName,
+			String baseUser, String basePass) throws ClassNotFoundException,
+			SQLException {
+		this.baseAddr = baseAddr;
+		this.baseName = baseName;
+		this.baseUser = baseUser;
+		this.basePass = basePass;
+		connection = establish_jdbc_connection();
+	}
+
+	private Connection establish_jdbc_connection()
+			throws ClassNotFoundException, SQLException {
+		String driverName = "com.mysql.jdbc.Driver";
+		Class.forName(driverName);
+		String url = "jdbc:mysql://" + baseAddr + "/" + baseName;
+		Connection connection = DriverManager.getConnection(url
+				+ "?useUnicode=true&characterEncoding=utf-8", baseUser,
+				basePass);
+		System.out.println("Connected to DB" + connection);
+		return connection;
+	}
+
+	public Statement createStatement() throws SQLException {
+		return connection.createStatement();
+	}
+
+	public PreparedStatement createPreparedStatement(String pquery)
+			throws SQLException {
+		return connection.prepareStatement(pquery);
+	}
+}
