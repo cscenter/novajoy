a04a39c72e05013b01bf227c29214179476eab1a
vovan <vovan@vovan-MacBook.(none)>
2013-03-29 17:28:16 +0400

    Config additions

--- src/django/novajoy/novajoy/settings.py
+++ src/django/novajoy/novajoy/settings.py
@@ -8,15 +8,16 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
-
+from novajoy.iniWorker import iniWorker
+config = iniWorker()
 DATABASES = {
     'default': {
-        'ENGINE': '****', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '****',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': config.getBase(),                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '****',
-        'PASSWORD': '****',
-        'HOST': '****',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'USER': config.getUser(),
+        'PASSWORD': config.getPassword(),
+        'HOST': config.getAddress(),                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
         'OPTIONS': {
             'init_command': 'SET names utf8',
