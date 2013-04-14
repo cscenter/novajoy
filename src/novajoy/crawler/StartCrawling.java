98064a9176ed68f31225f28867e8ed3b81a914d0
Roman <filiroman.tsu@gmail.com>
2013-04-14 14:50:16 +0400

    no message

--- src/novajoy/crawler/StartCrawling.java
+++ src/novajoy/crawler/StartCrawling.java
@@ -6,7 +6,7 @@ import novajoy.util.db.JdbcManager;
 public class StartCrawling {
 	public static void main(String[] args) throws Exception {
 		IniWorker config = new IniWorker(
-				"/home/ubuntu/NovaJoy/config/config.ini");
+                "/home/ubuntu/NovaJoy/config/config.ini");
 		JdbcManager dbman = new JdbcManager(config.getDBaddress(),
 				config.getDBbasename(), config.getDBuser(),
 				config.getDBpassword());
