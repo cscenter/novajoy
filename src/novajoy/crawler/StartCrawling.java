9564b9181d3b5dc7fe78da9b41819abe07fcd5f3
filiroman <filiroman.tsu@gmail.com>
2013-04-21 16:11:33 +0400

    Update StartCrawling.java

--- src/novajoy/crawler/StartCrawling.java
+++ src/novajoy/crawler/StartCrawling.java
@@ -6,7 +6,7 @@ import novajoy.util.db.JdbcManager;
 public class StartCrawling {
 	public static void main(String[] args) throws Exception {
 		IniWorker config = new IniWorker(
-                "/home/ubuntu/NovaJoy/novajoy/config/config.ini");
+                "/home/ubuntu/NovaJoyConfig/config.ini");
 		JdbcManager dbman = new JdbcManager(config.getDBaddress(),
 				config.getDBbasename(), config.getDBuser(),
 				config.getDBpassword());
