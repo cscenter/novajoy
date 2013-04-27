b781b4e12d2ec4ba47e0238532b03723198c3402
vovan <cska631@gmail.com>
2013-04-27 21:45:53 +0400

    Current collection a allocated by red color

--- src/novajoy/django/novajoy/static/JS/base.js
+++ src/novajoy/django/novajoy/static/JS/base.js
@@ -4,8 +4,7 @@ function send(nameOfNewCollection,updateInterval,sendingTime,format,subject){
     var isAdding = "No";
     curCol = nameOfNewCollection;
 
-    //alert("Hello");
-    $('.collection span:last').click();
+    //$('.collection span:last').click();
     $.post('/addCollection/', {newCollection: nameOfNewCollection, updateInterval: updateInterval,sendingTime:sendingTime,format:format, subject:subject},
         function (data) {
             var response = data;
@@ -15,13 +14,15 @@ function send(nameOfNewCollection,updateInterval,sendingTime,format,subject){
                     clickCollection($(this).text());
                 });
                 curCol = nameOfNewCollection;
-		$('.collection span:last').click();
+                $('.collection span:last').click();
+                curObject = $('.collection span:last') ;
+                $('.collection span:last').click();
             } else {
                 alert(response);
             }
         }
     );
-    curObject = $('.collection span:last') ;
+
 //    alert($('.collection span:last').text());
     //$('.collection span:last').click();
 }
@@ -30,14 +31,12 @@ function sendEditCollection(oldName,nameOfNewCollection,updateInterval,sendingTi
     $.post('/editCollection/', {oldName:oldName,newCollection: nameOfNewCollection, updateInterval: updateInterval,sendingTime:sendingTime,format:format, subject:subject},
         function (data) {
             var response = data;
-  //          alert("hello "+response);
             if (response == "Success") {
                 //<div><p><span title='{{ col }}'>{{ col }}</span></div>
                 curObject = $('.collection div:contains('+oldName+') p span').text(nameOfNewCollection);
                 curCol=nameOfNewCollection;
                 curObject.click();
-               // alert($('.collection div:contains('+oldName+')').text());
-                //curCol = nameCollection;
+                // alert($('.collection div:contains('+oldName+')').text());
             }
             if(response=="Error/this name already exist"){
                 alert("Error/this name already exist");
@@ -58,54 +57,54 @@ function showDialog(func,old_name) {
             document.getElementById('myform').reset();
         },
         open:function(){
-          if (old_name!=""){
+            if (old_name!=""){
 //              document.forms[0].elements[0].value=curCol;
 
-              $.post('/infoAboutCollection/', {oldName:old_name},
-                  function (data) {
-                      var response = $.parseJSON(data);
-                      document.forms[0].elements[0].value = response[0]['fields']['name_collection'];
-                      document.forms[0].elements[4].value = response[0]['fields']['subject'];
-                      //alert(response[0]['fields']['delta_update_time']);
-                      document.forms[0].elements[3].options[format_map[response[0]['fields']['format']]].selected=true;
-                      document.forms[0].elements[1].options[update_time_map[response[0]['fields']['delta_update_time']]].selected=true;
-                      //alert(response[0]['fields']['subject']);
-                  }
-              );
-          }
+                $.post('/infoAboutCollection/', {oldName:old_name},
+                    function (data) {
+                        var response = $.parseJSON(data);
+                        document.forms[0].elements[0].value = response[0]['fields']['name_collection'];
+                        document.forms[0].elements[4].value = response[0]['fields']['subject'];
+                        alert(response[0]['fields']['sendingTime']);
+                        document.forms[0].elements[3].options[format_map[response[0]['fields']['format']]].selected=true;
+                        document.forms[0].elements[1].options[update_time_map[response[0]['fields']['delta_update_time']]].selected=true;
+                        //alert(response[0]['fields']['subject']);
+                    }
+                );
+            }
         },
         buttons: {
-        OK: function () {
-            //Data from a form
-            var nameOfNewCollection = document.forms[0].elements[0].value;
-            var updateInterval = document.forms[0].elements[1].value;
-            var sendingTime = document.forms[0].elements[2].value;
-            var format = document.forms[0].elements[3].value;
-            var subject = document.forms[0].elements[4].value;
-            //clean form
-            document.getElementById('myform').reset();
-            nameOfNewCollection = nameOfNewCollection.trim();
-            var isnan = isNaN(parseInt(updateInterval));
-            if (nameOfNewCollection == "" || subject.trim()=="" ) {
-                alert("Empty field");
-            } else{
-                if(old_name!=''){
-                    alert("Edit");
-                    func(old_name,nameOfNewCollection,updateInterval,sendingTime,format,subject);
-                }else{
-                    alert("new");
-                    func(nameOfNewCollection,updateInterval,sendingTime,format,subject);
+            OK: function () {
+                //Data from a form
+                var nameOfNewCollection = document.forms[0].elements[0].value;
+                var updateInterval = document.forms[0].elements[1].value;
+                var sendingTime = document.forms[0].elements[2].value;
+                var format = document.forms[0].elements[3].value;
+                var subject = document.forms[0].elements[4].value;
+                //clean form
+                document.getElementById('myform').reset();
+                nameOfNewCollection = nameOfNewCollection.trim();
+                var isnan = isNaN(parseInt(updateInterval));
+                if (nameOfNewCollection == "" || subject.trim()=="" ) {
+                    alert("Empty field");
+                } else{
+                    if(old_name!=''){
+                        //alert("Edit");
+                        func(old_name,nameOfNewCollection,updateInterval,sendingTime,format,subject);
+                    }else{
+                        //alert("new");
+                        func(nameOfNewCollection,updateInterval,sendingTime,format,subject);
+                    }
                 }
+                $(this).dialog("close");
+                return false;
+            },
+            Cancel: function () {
+                document.getElementById('myform').reset();
+                $(this).dialog("close");
+                return false;
             }
-            $(this).dialog("close");
-            return false;
-        },
-        Cancel: function () {
-            document.getElementById('myform').reset();
-            $(this).dialog("close");
-            return false;
         }
-    }
     });
     $("#dialog1").dialog("open");
 }
@@ -142,15 +141,21 @@ function clickRemoveCollection(){
 function clickCollection(text) {
     $('.listURL span').remove();
     var nameCollection = text;
-
+    if(curObject!=null){
+        curObject.removeAttr('id');
+    }
+    else{
+        alert("vova");
+    }
     curObject = $('.collection div:contains('+text+')');
     curCol = nameCollection;
+    curObject.attr('id','hello');
+    $(".listURL h2").remove();
+    $(".link").remove();
+    $(".listURL").prepend('<h2>' + curCol + '</h2>');
     $.post('/selectURL/', {nameCollection: nameCollection},
         function (data) {
             var tmp = $.parseJSON(data);
-            $(".listURL h2").remove();
-            $(".link").remove();
-            $(".listURL").prepend('<h2>' + curCol + '</h2>');
             for (var i = 0; i < tmp.length; i++) {
                 var url = tmp[i]['fields']['url'];
                 var tt = "<div class='link'><p><span>" + url +
@@ -176,7 +181,7 @@ function deleteRSS(url){
             var response = data;
 
             if (response == "Success") {
-               // alert($('.listURL div:contains('+$(this).attr('href')+')').length);
+                // alert($('.listURL div:contains('+$(this).attr('href')+')').length);
                 //alert($('.link div:contains('+$(this).attr('href')+')').length);
                 $('.listURL div:contains('+url+')').remove();
             } else {
@@ -231,17 +236,21 @@ $(document).ready(function () {
     update_time_map['7200']=3;
     update_time_map['14400']=4;
 
+    sending_time_map=[];
+    sending_time_map['']
     $(".collection span").bind('click',function(evt){
         evt.preventDefault();
-        curObject =$(this);
         clickCollection($(this).text());
     });
     if ($('.collection span').length == 0) {
         curCol = "You have no collections";
+        curObject=null;
         $(".listURL").prepend('<h2>' + curCol + '</h2>');
     } else {
-        curCol = $('.collection span:first').text()
-        $('.collection span:first').click();
+        curCol = $('.collection span:first').text();
+        curObject =$('.collection span:first');
+        curObject.click();
     }
 
 });
+
